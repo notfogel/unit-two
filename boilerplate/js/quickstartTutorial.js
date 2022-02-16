779a039c0ef2f070661175aa1274a0353b40208b
Sammy <98495588+notfogel@users.noreply.github.com>
2022-02-16 17:20:19 -0600

    Update quickstartTutorial.js

--- boilerplate/js/quickstartTutorial.js
+++ boilerplate/js/quickstartTutorial.js
@@ -8,10 +8,23 @@ var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 500
+    radius: 550
 }).addTo(map);
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a birble.");
+polygon.bindPopup("I am a polygon.");
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You blicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
