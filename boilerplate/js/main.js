05b72e0cb95d576b16ccb81ca0416866f54cec72
Sammy <98495588+notfogel@users.noreply.github.com>
2022-02-23 17:50:45 -0600

    main.js is a leaflet map with the geoJSON!
    Activity 5 baby

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,4 +1,51 @@
-var mydiv = document.getElementById("mydiv");
-mydiv.addEventListener("click", function(){
-    alert("Hello Borld! boooooooooooooooooooooooooooooooo");
-})
+var map;
+
+function createMap(){
+   map = L.map('map', {
+        center: [40,-98.5],
+        zoom: 3
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+    //call the uh, getData fxn
+    getData();
+};
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+function getData(){
+    fetch("data/antitranslaws_15gap_17gap.geojson")
+        .then(function(response){
+            return response.json();
+    })
+    .then(function(json){
+        var geojsonMarkerOptions = {
+            radius: 8,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+        
+        L.geoJSON(json, {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            },        
+            onEachFeature: onEachFeature
+            
+        }).addTo(map);
+    });     
+};
+//the line below this one activates all the stuff above this line once the DOM hath loaded
+document.addEventListener('DOMContentLoaded',createMap)
+
